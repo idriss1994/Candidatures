d1e234f101640eb413ba2d7ba77a3abd6809d3e5
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-11-17 20:30:08 +0100

    Add project files.

---
+++ Candidatures/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Candidatures.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
