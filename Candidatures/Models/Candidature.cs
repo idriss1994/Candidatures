be5d51bb8282c594b16ee878c920491a6ac5c603
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-11-17 20:46:45 +0100

    Create candidature model

---
+++ Candidatures/Models/Candidature.cs
@@ -0,0 +1,15 @@
+﻿namespace Candidatures.Models
+{
+    public class Candidature
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string LevelOfStudy { get; set; }
+        public int NumberOfYearsOfExperience { get; set; }
+        public string LastEmployer { get; set; }
+
+    }
+}
