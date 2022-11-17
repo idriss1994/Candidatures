4e9244667498aaa7ab60a06f243a15c5d3caad72
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-11-17 21:29:44 +0100

    Create Candidature DbContext

---
+++ Candidatures/Data/CandidatureDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Candidatures.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Candidatures.Data
+{
+    public class CandidatureDbContext : DbContext
+    {
+        public CandidatureDbContext(DbContextOptions<CandidatureDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Candidature> Candidatures { get; set; }
+    }
+}
