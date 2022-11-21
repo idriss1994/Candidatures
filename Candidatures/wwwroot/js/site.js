47de22d34ef97915f8d375b5f8678674e74b6f59
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-11-21 18:54:36 +0100

    Add confirmation deletion popup

--- Candidatures/wwwroot/js/site.js
+++ Candidatures/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// $(document).ready(function() {
+
+//     $(".btn-delete").click = () => {
+        
+//     }
+// });
+
+let buttonsDelete = document.querySelectorAll('.btn-delete');
+let btnClose = document.querySelector('.btn-no');
+
+buttonsDelete.forEach(btn => btn.addEventListener("click", () => {
+    document.querySelector('#pop').classList.add('open');
+}));
+
+btnClose.addEventListener("click", () => {
+    document.querySelector('#pop').classList.remove('open');
+});
